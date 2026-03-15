8d29e925587b745cdd9de32c6145cb7a780c901a
AbiSelviS <abiselvis154@gmail.com>
2026-03-15 19:30:09 +0530

    Update Experience.jsx

--- Frontend/src/components/Experience.jsx
+++ Frontend/src/components/Experience.jsx
@@ -4,18 +4,16 @@ import ABI from "../assets/abi1.jpeg";
 import alo from "../assets/alo.jpg";
 import infopark from "../assets/ak2.jpg";
 import orvionar from "../assets/orvionar.jpg";
-import resumee from "../assets/AbiSelvi.Resume.pdf";
 
 
 
 const Experience = () => {
   return (
     <div className={Exper.exp}id="Experience" >
-     <div className={Exper.expbg}>
+
+      <div className={Exper.expbg}>
         <button className={Exper.button1} >view more</button>
       </div>
-  
-
 
       <div className={Exper.profileimg}>
         <img src={ABI} alt="abi"  className={Exper.imaged}/>
@@ -23,7 +21,7 @@ const Experience = () => {
 
       <div className={Exper.about}>
         <h1>Experience</h1>
-      <a href={resumee} className={Exper.but} > View My Resume</a> 
+        <button> View My Resume</button>
       </div>
 
       <div className={Exper.experiencecontainer}>
