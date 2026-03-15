a6ea8068e12f2484bef340cf1f398721cbaf9096
AbiSelviS <abiselvis154@gmail.com>
2026-03-15 17:32:00 +0530

    Update HeroSection.jsx

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -1,5 +1,7 @@
 
 
+
+     
 import React, { useEffect } from "react";
 import picture from "../assets/abi1.jpeg";
 import Hero from './HeroSection.module.css';
@@ -12,50 +14,59 @@ const HeroSection = () => {
 
   useEffect(() => {
     AOS.init({
-      duration: 1200, // animation duration
-      once: false,    // animate every time it comes into view
+      duration: 1200,
+      once: false,
     });
-    AOS.refresh();     // ensures AOS recalculates positions
+    AOS.refresh();
   }, []);
 
   return (
     <div className={Hero.hero} id="About">
 
-      {/* Left Side: Text */}
+      {/* Left Side */}
       <div className={Hero.heroleft} data-aos="fade-right">
         <p className={Hero.intro}>Hi there, I am</p>
+
         <h1 className={Hero.name}>S. Abi Selvi</h1>
+
         <h2 className={Hero.role}>Frontend Developer</h2>
+
         <p className={Hero.desc}>
           Aspiring Frontend Developer passionate about building responsive
           and user-friendly websites using HTML, CSS, JavaScript, and React.
         </p>
 
-        {/* Buttons and Social Icons */}
+        {/* Button + Icons */}
         <div className={Hero.herobuttons}>
-          <a href="#Contact"><button className={Hero.contact}>Contact Me</button></a>
-          <button className={Hero.cv}>Download CV</button> 
 
+          {/* Download CV Button */}
+            <a href="" className={Hero.cv}>
+          Download CV
+          </a>
+
+          {/* Github */}
           <a href="https://github.com/AbiSelviS" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon 
-              icon={faGithub} 
-              size="1x" 
-              color="#ece7e7" 
-              style={{ marginRight: "20px" }}
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="1x"
+              color="#ece7e7"
+              style={{ marginLeft: "20px", marginRight: "20px" }}
             />
           </a>
 
+          {/* Linkedin */}
           <a href="https://www.linkedin.com/in/s-abi-selvi-915b85259" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon 
-              icon={faLinkedin} 
-              size="1x" 
-              color="#d7dbdf" 
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="1x"
+              color="#d7dbdf"
             />
           </a>
+
         </div>
       </div>
 
-      {/* Right Side: Image */}
+      {/* Right Side Image */}
       <div className={Hero.heroright} data-aos="fade-left" data-aos-delay="200">
         <div className={Hero.imagebg}>
           <img src={picture} alt="Abi Selvi" />
